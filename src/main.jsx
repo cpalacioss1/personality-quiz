99eeff673c252c16fa72ed665516a250738d4567
cpalacioss1 <cpalacios.arg@gmail.com>
2025-11-23 12:40:24 -0300

    fix(context): wrap App with UserProvider in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { UserProvider } from './components/UserContext';
 import App from './App';
 import './index.css';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
 	<BrowserRouter>
-		<App />
+		<UserProvider>
+			<App />
+		</UserProvider>
 	</BrowserRouter>
 );
